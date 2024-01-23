582621cdf38d2525102cee7b18a63944debdf35d
NO Internet <128231475+Dogeisveryop@users.noreply.github.com>
2024-01-23 21:54:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+monkey = "";
+meandmymonkey = "";
+rightWrist_x = 0;
+rightWrist_y = 0;
+leftWrist_x = 0;
+leftWrist_y = 0;
+scoreleftWrist = 0;
+song_name = "";
+
+function setup(){
+    canvas = createCanvas(600,490);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+
+function preload(){
+    monkey = loadSound("monkey.mp3");
+    meandmymonkey = loadSound("meandmymonkey.mp3");
+}
+
+function draw(){
+    image(video,0,0,600,490);
+
+    fill("darkblue");
+    stroke("black");
+
+    song_name = monkey.isPlaying();
+    console.log(song_name);
+
+    if(scoreleftWrist > 0.2){
+        circle(leftWrist_x,leftWrist_y,20);
+        monkey.stop();
+        if(song_name == false){
+            meandmymonkey.play();
+            console.log("Now playing Spining monkey");
+            document.getElementById("songh").innerHTML = "Now playing Spining monkey";
+        }
+        
+    }
+}
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized");
+}
+
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+
+        scoreleftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreleftWrist);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        leftWrist_y = results[0].pose.leftWrist.y;
+        console.log("leftWrist_x = "+leftWrist_x+" leftWrist_y = "+leftWrist_y);
+
+        rightWrist_x = results[0].pose.rightWrist.x;
+        rightWrist_y = results[0].pose.rightWrist.y;
+        console.log("rightWrist_x = "+rightWrist_x+" rightWrist_y = "+rightWrist_y);
+    }
+}
